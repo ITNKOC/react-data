57332535eeee5e341f8886054cd0ca0372425ca0
ITNKOC <koceila.djaballah@gmail.com>
2024-04-14 21:13:02 -0400

    first commit

---
+++ backend/data/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    _id: 1,
+    email: "lilia@react.teccart",
+    password: "1234",
+    fullName: "Lilia Ould Hocine",
+  },
+  {
+    _id: 2,
+    email: "beyonce@react.teccart",
+    password: "1234",
+    fullName: "Beyoncé Knowles",
+  },
+];
+
+exports.module = { users };
